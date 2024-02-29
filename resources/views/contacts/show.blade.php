2251cbf08bfb1a3612502317c0ffd8b494125fd2
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-29 16:27:51 +0900

    README修正

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -75,7 +75,7 @@
                                         </div>
                                     </div>
                                     <div class="p-2 w-full">
-                                        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">送信</button>
+                                        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><a href="{{route('contacts.edit', ['contact', $contact->id])}}"></a></button>
                                     </div>
                                 </div>
                             </div>
