6af78fa990bdd9800ac1d89cd70c79a3646c5b82
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-29 14:06:23 +0900

    tailblockから登録フォーム実装

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,1 +1,64 @@
-<h1>新規登録フォーム</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            新規お問い合わせ登録フォーム
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <section class="text-gray-600 body-font relative">
+                        <div class="container px-5 py-10 mx-auto">
+                            <div class="flex flex-col text-center w-full mb-12">
+                                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">お問い合わせ内容</h1>
+                                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">できるだけ詳細に御入力お願いいたします</p>
+                            </div>
+                            <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                <div class="flex flex-wrap">
+                                    <div class="p-2 w-full">
+                                        <div class="relative">
+                                            <label for="name" class="leading-7 text-sm text-gray-600">お名前</label>
+                                            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+                                    <div class="p-2 w-full">
+                                        <div class="relative">
+                                            <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
+                                            <input type="text" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+
+                                    <div class="p-2 w-full">
+                                        <div class="relative">
+                                            <label for="title" class="leading-7 text-sm text-gray-600">タイトル</label>
+                                            <input type="text" id="title" name="title" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+                                    <div class="p-2 w-full">
+                                        <div class="relative">
+                                            <label for="name" class="leading-7 text-sm text-gray-600">お名前</label>
+                                            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+
+                                    <div class="p-2 w-full">
+                                        <div class="relative">
+                                            <label for="message" class="leading-7 text-sm text-gray-600">具体的な内容</label>
+                                            <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="p-2 w-full">
+                                        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">送信</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <a href="{{route('contacts.create')}}">新規登録</a>
+                    <a href="{{route('contacts.create')}}" class="text-indigo-600 font-semibold">新規登録</a>
                 </div>
             </div>
         </div>
